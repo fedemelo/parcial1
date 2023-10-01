4595301463bb527c31d8d83ae070875ce779da18
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-10-01 10:57:00 -0500

    Kleinere Korrektur der Fußzeile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,6 @@ export default function Footer() {
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&family=Inter:wght@400;700;900&family=Nunito:wght@300;400&family=Open+Sans:wght@300;400&family=Poppins:wght@300;500&family=Space+Grotesk&display=swap');
         </style>
-        <p id="footer" className="text-center"><FormattedMessage id="contactUs" />: +57 3102105253 - info@elaromagico.com - @elaromagico</p>
+        <p id="footer" className="text-center"><FormattedMessage id="contactUs" />: <FormattedMessage id="phoneNum" /> - <FormattedMessage id="email" /> - <FormattedMessage id="insta" /></p>
     </Container>
 }
